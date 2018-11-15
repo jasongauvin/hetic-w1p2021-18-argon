86fde289e1da8056f44323c82b8ae6c73df2f9d2
lenapoleon <contact.jason.gauvin@gmail.com>
2018-11-15 19:46:18 +0100

    jeudimaj2

--- js/app.js
+++ js/app.js
@@ -29,24 +29,42 @@ open_menu.addEventListener('click', function () {
 
 var open_products = document.getElementById('products');
 var open_articles = document.getElementById('articles');
+var history_is_open = false;
+var articles_is_open = false;
 
 var open_history = document.getElementById('history');
 var open_story = document.getElementById('title-story');
+
+
 open_history.addEventListener('click', function () {
+  history_is_open = !history_is_open;
   anchor.classList.toggle('is-open-story');
   open_history.classList.toggle('is-open-history');
-  open_products.classList.toggle('is-open-products');
-  open_articles.classList.toggle('is-open-products');
-
+  if ((history_is_open || articles_is_open) || (!history_is_open && !articles_is_open)) {
+    open_products.classList.remove('is-open-products');
+    open_articles.classList.remove('is-open-products');
+  } else {
+    open_products.classList.add('is-open-products');
+    open_articles.classList.add('is-open-products');
+  }
 
 });
+
 
 var open_values = document.getElementById('values');
 var values_para = document.getElementById('values-para');
-open_values.addEventListener('click', function () {
+
+  open_values.addEventListener('click', function () {
+  articles_is_open = !articles_is_open
   values_para.classList.toggle('is-open-para');
   open_values.classList.toggle('is-open-values');
-  open_products.classList.toggle('is-open-products');
-  open_articles.classList.toggle('is-open-products');
-
+  if ((history_is_open || articles_is_open) || (!history_is_open && !articles_is_open)) {
+    open_products.classList.remove('is-open-products');
+    open_articles.classList.remove('is-open-products');
+  } else {
+    open_products.classList.add('is-open-products');
+    open_articles.classList.add('is-open-products');
+  }
 });
+
+
